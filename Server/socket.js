aa5cf16f5a053e5f7a2cee816ffae54f5b6ee821
Madhav Jangid <manumadhavjangid@gmail.com>
2024-06-26 19:01:23 +0530

    Update socket.js

--- Server/socket.js
+++ Server/socket.js
@@ -1,15 +1,26 @@
-const { Server } = require('socket.io');
-
 
 function setupSocket(httpServer) {
     const io = new Server(httpServer, { cors: { origin: "*", methods: ["GET", "POST"] } });
 
     io.on("connection", (socket) => {
+        console.log(`New client connected: ${socket.id}`);
 
         socket.on('sent_message', (data) => {
+            console.log(`Message received: ${data}`);
             io.emit('get_message', data);
         });
 
+        socket.on('disconnect', (reason) => {
+            console.log(`Client disconnected: ${socket.id}, reason: ${reason}`);
+        });
+
+        socket.on('error', (error) => {
+            console.error(`Socket error: ${error}`);
+        });
+    });
+
+    io.on('error', (error) => {
+        console.error(`Socket.io error: ${error}`);
     });
 
     return io;
